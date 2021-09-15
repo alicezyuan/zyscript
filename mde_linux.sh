dd77dba55dd819cbbf89ee01d1dcbad03d262f18
alicezyuan <40851466+alicezyuan@users.noreply.github.com>
2021-09-15 17:54:02 +0800

    Update mde_linux.sh

--- mde_linux.sh
+++ mde_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo yum install yum-utils
+sudo yum install yum-utils -y
 sudo yum-config-manager --add-repo=https://packages.microsoft.com/config/rhel/7/prod.repo
 sudo rpm --import http://packages.microsoft.com/keys/microsoft.asc
 yum makecache
-sudo yum install mdatp
+sudo yum install mdatp -y
 wget https://alicesap.blob.core.windows.net/suseiso/MicrosoftDefenderATPOnboardingLinuxServer.py
 sudo chmod +x MicrosoftDefenderATPOnboardingLinuxServer.py
 sudo python3 MicrosoftDefenderATPOnboardingLinuxServer.py
